c42766ab530873ef0f12f262aa568e4739acf842
Abbeyjp <>
2023-10-12 08:04:08 +0000

    Intoduced the functionality for updating the sales entry

--- run.py
+++ run.py
@@ -4,7 +4,7 @@
 
 import gspread
 from google.oauth2.service_account import Credentials
-from datetime import datetime
+from datetime import date, time, datetime
 from pprint import pprint
 
 
@@ -29,10 +29,19 @@ def get_last_five_sales_entries():
 
 
 def update_last_sales_entries(s):
+    """
+    This funtion is either to input today's sales entry or edit
+    """
     column = s[-1]
     print(column[0])
     date_object = datetime.strptime(column[0], '%m/%d/%Y').date()
-    print(type(date_object))
+    today = date.today()
+    if today==date_object:
+         """
+         Checking if the data was already inputted
+         """
+        print("Todays update was already entered:/n", column, "/nWould you like to edit if yes please enter '1' and if no please enter '2'")
+        opt1 = input()
     
 
 
@@ -53,7 +62,7 @@ def main():
    print("Please enter the an option '1' for updating today's sale, '2' for printing the last 5 day sales, '3' for printing the upcoming stock update")
    opt = input()
    if opt=='1':
-    print(type(opt))
+    update_last_sales_entries(sales)
    elif opt=='2':
     get_last_five_sales_entries()
    elif opt=='3':
