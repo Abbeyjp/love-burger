2109096529fb05ed1d25c706a3e87f61b44ff4e9
Abbeyjp <>
2023-10-13 06:03:50 +0000

    Added and completed the append function which adds the new data

--- run.py
+++ run.py
@@ -20,67 +20,13 @@ GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 SHEET = GSPREAD_CLIENT.open('love_burger')
 sales = SHEET.worksheet("sales").get_all_values()
 stock = SHEET.worksheet("stocks").get_all_values()
+today = date.today()
 
 def get_last_five_sales_entries():
     """
     print the last 5 sales entries
     """
     print(sales[-5:])
-
-
-def update_last_sales_entries(s):
-    """
-    This funtion is either to input today's sales entry or edit
-    """
-    column = s[-1]
-    print(column[0])
-    date_object = datetime.strptime(column[0], '%m/%d/%Y').date()
-    today = date.today()
-    if today==date_object:
-        """
-         Checking if the data was already inputted
-        """
-        edit_last_input(column,len(s))
-    elif (today-date_object) == timedelta(days = 1):
-        """
-         Inputting the new line to the sales list & update the stock
-        """
-        print("append")
-        append_today_sales()
-    else:
-        """
-         Automating to fill up for the rest of the dates between today and last usage of the application
-        """
-        print(today-date_object,tday)
-    
-
-def edit_last_input(c, l):
-    print("Todays update was already entered:\n", c, "\nWould you like to edit if yes please enter '1' and if not enter anykey")
-    opt1 = input()
-    if opt1 == '1':
-        """
-         We will edit the last row
-        """
-        SHEET.worksheet("sales").delete_rows(l)
-        append_today_sales()
-
-def validate_data(val):
-    """
-    This is to validate the data by converting the string to the numbers and followed by checking
-    """
-    try:
-        [int(v) for v in val]
-        if len(val) != 13:
-            raise ValueError(
-              f"Exactly 13 values required, provided {len(val)}"     
-             )
-    except ValueError as e:
-        print(f"Invalid data: {e}, please try again.\n")
-        return False
-
-    return True
-
-
 
 def append_today_sales():
     """
@@ -96,9 +42,64 @@ def append_today_sales():
             print("Data valid")
             break
     
-    return sales_data
+    sales_data = [today.strftime("%m/%d/%Y")]+sales_data
+    SHEET.worksheet("sales").append_row(sales_data)
+    print(sales_data)
 
 
+def update_last_sales_entries(s):
+    """
+    This funtion is either to input today's sales entry or edit
+    """
+    column = s[-1]
+    print(column[0])
+    date_object = datetime.strptime(column[0], '%m/%d/%Y').date()
+    
+    if today==date_object:
+        """
+         Checking if the data was already inputted
+        """
+        edit_last_input(column,len(s))
+    elif (today-date_object) == timedelta(days = 1):
+        """
+         Inputting the new line to the sales list & update the stock
+        """
+        print("append")
+        append_today_sales()
+    else:
+        """
+         Automating to fill up for the rest of the dates between today and last usage of the application
+        """
+        print(today-date_object,today)
+    
+
+def edit_last_input(c, l):
+    print("Todays update was already entered:\n", c, "\nWould you like to edit if yes please enter '1' and if not enter anykey")
+    opt1 = input()
+    if opt1 == '1':
+        """
+         We will edit the last row
+        """
+        SHEET.worksheet("sales").delete_rows(l)
+        append_today_sales()
+    else:
+        print("Process cancelled......")
+
+def validate_data(val):
+    """
+    This is to validate the data by converting the string to the numbers and followed by checking. This also checks whether there are 13 figures entered.
+    """
+    try:
+        [int(v) for v in val]
+        if len(val) != 13:
+            raise ValueError(
+              f"Exactly 13 values required, provided {len(val)}"     
+             )
+    except ValueError as e:
+        print(f"Invalid data: {e}, please try again.\n")
+        return False
+
+    return True
 
 
 
